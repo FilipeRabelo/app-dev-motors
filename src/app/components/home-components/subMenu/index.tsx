ff2fe1fcfabbcf83dc657f8a5bd01017e97c6206
Filipe Rabelo F Lana <filipe.rabelolana@hotmail.com>
2024-11-16 22:27:17 -0300

    app-next-dev-motors

--- src/app/components/home-components/subMenu/index.tsx
+++ src/app/components/home-components/subMenu/index.tsx
@@ -4,25 +4,27 @@ import { X, Menu } from 'lucide-react';
 
 export function SubMenu() {
   return (
-    <section className={styles.subMenu}>
+    <div className={styles.divSubMenu}>
+      <section className={styles.subMenu}>
 
-      <div className={styles.subMenuIcon}>
-        <Menu size={34} color="#121212"/>
-        Menu
-      </div>
+        <div className={styles.subMenuIcon}>
+          <Menu size={34} color="#121212" />
+          Menu
+        </div>
 
-      <ul className={styles.ul}>
-        <li>
-          <Link href={"/post/pagina-1"}>
-            Pagina 1
-          </Link>
+        <ul className={styles.ul}>
+          <li>
+            <Link href={"/post/pagina-1"}>
+              Pagina 1
+            </Link>
 
-          <Link href={"/post/pagina-2"}>
-            Pagina 2
-          </Link>
-        </li>
-      </ul>
+            <Link href={"/post/pagina-2"}>
+              Pagina 2
+            </Link>
+          </li>
+        </ul>
 
-    </section>
+      </section>
+    </div>
   );
 }
